634fc2024c06d2ee4b2fc8a7f1bf07c0efbbbffc
Roshan G <rg520046@gmail.com>
2026-01-09 22:30:50 +0530

    Add files via upload

---
+++ PythonProject7/Ball.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.goto(0,0)
+        self.xmove=10
+        self.ymove=10
+        self.move_speed=0.1
+
+    def move(self):
+        new_x = self.xcor() + self.xmove
+        new_y = self.ycor() + self.ymove
+        self.goto(new_x,new_y)
+
+    def bounce_y(self):
+        self.ymove*= -1
+
+    def bounce_x(self):
+        self.xmove*= -1
+        self.move_speed *=0.8
+
+    def reset_ball(self):
+        self.goto(0,0)
+
+
+
+
+
